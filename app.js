0695bdfbf6170d7ed2a31428aa03f53031604f3e
Noah2211 <noah.decroix3@gmail.com>
2021-11-10 15:52:48 +0100

    Almost finished exercices

---
+++ app.js
@@ -0,0 +1,46 @@
+let colorP1 = document.querySelector("#p1");
+colorP1.style.color = "#ff0000";
+
+let colorP2 = document.querySelector("#p2");
+colorP2.style.color = "#ff0000";
+
+let colorP3 = document.querySelector("#p3");
+colorP3.style.color = "#ff0000";
+
+let colorP4 = document.querySelector("#p4");
+colorP4.style.color = "#ff0000";
+
+let colorP5 = document.querySelector("#p5");
+colorP5.style.color = "#ff0000";
+
+
+
+let bgSpan1 = document.getElementById("SpanTest1")
+bgSpan1.style.background = "#ff0000";
+
+let bgSpan2 = document.getElementById("SpanTest2")
+bgSpan2.style.background = "#ff0000";
+
+let bgSpan3 = document.getElementById("SpanTest3")
+bgSpan3.style.background = "#ff0000";
+
+let bgSpan4 = document.getElementById("SpanTest4")
+bgSpan4.style.background = "#ff0000";
+
+let bgSpan5 = document.getElementById("SpanTest5")
+bgSpan5.style.background = "#ff0000";
+
+let bgSpan6 = document.getElementById("SpanTest6")
+bgSpan6.style.background = "#ff0000";
+
+let bgSpan7 = document.getElementById("SpanTest7")
+bgSpan7.style.background = "#ff0000";
+
+let bgSpan8 = document.getElementById("SpanTest8")
+bgSpan8.style.background = "#ff0000";
+
+let bgSpan9 = document.getElementById("SpanTest9")
+bgSpan9.style.background = "#ff0000";
+
+let bgSpan10 = document.getElementById("SpanTest10")
+bgSpan10.style.background = "#ff0000";
